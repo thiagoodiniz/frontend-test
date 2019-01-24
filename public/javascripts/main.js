598106806c74bf70d0056acb47ddcea27eca5795
Thiago Diniz de Sousa <thiago.diniz@sysmap.com.br>
2019-01-23 22:29:06 -0200

    feat: getting json file

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,11 @@
-// your code
+function getJson(){
+    var obj = new XMLHttpRequest();
+    obj.overrideMimeType("application/json");
+    obj.open('GET', 'fazenda.json', false); 
+    obj.send();
+    if (obj.readyState == 4 && obj.status == "200") {
+        return JSON.parse(obj.responseText)
+    }   
+}
+
+const json = getJson()
