3f57e187484d9ddf2bb9563ba3354834680505ad
Thiago Diniz de Sousa <thiago.diniz@sysmap.com.br>
2019-01-23 22:56:15 -0200

    feat: calculating percentage; showing the arrow when mouseover and hiding it when mouseout

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -13,7 +13,7 @@ function render(json){
         ${
             json.data.map(item => 
                 `
-                <li class="list-item" onmouseover="renderArrowPercents()" id="${item.__id}">
+                <li class="list-item" onmouseover="hoverItemList(this, ${item.positive}, ${item.negative})" onmouseout="removeArrow()" id="${item.__id}">
                     <img class="img" src="${item.picture}" alt="...">
                     <div class="text"> 
                         <h3>${item.name}</h3>
@@ -26,16 +26,34 @@ function render(json){
         `
 }
 
-function renderArrowPercents(){
+function hoverItemList(element, positive, negative){
+    let percents = calculatePercentage(positive, negative);
+    let arrow = document.getElementById('arrow');
+    arrow.innerHTML = renderArrowPercents(percents);
+    arrow.style.display = 'block';
+}
+
+function removeArrow(){
+    document.getElementById('arrow').style.display = 'none';
+}
+
+function calculatePercentage(positive, negative){
+    let total = positive + negative;
+    positive = Math.round((positive / total) * 100);
+    negative = Math.round((negative / total) * 100);
+    return {positive, negative} 
+}
+
+function renderArrowPercents(percents){
     return  `
             <div class="into-arrow">
                 <p class="p1">Gostam</p>
                 <p class="p2">Não Gostam</p>
                 <div class="percent-one">
-                    <p>40%</p>
+                    <p>${percents.positive}%</p>
                 </div>
                 <div class="percent-two">
-                    <p>60%</p>
+                    <p>${percents.negative}%</p>
                 </div>
             </div>
            `
